e018b95339535f2ee700ff12fb60734e219606fc
Shaan <shaan@shaanstraining.com>
2019-04-14 10:30:43 -0500

    updated test suites

--- src/test/java/com/shaanstraining/api/TranslateAPITest.java
+++ src/test/java/com/shaanstraining/api/TranslateAPITest.java
@@ -1,6 +1,7 @@
 package com.shaanstraining.api;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import java.io.FileReader;
 
@@ -8,34 +9,43 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.testng.annotations.Test;
 
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-
 public class TranslateAPITest {
 	static final String JSON_FILE = "src/test/resources/translate-api.json";
-	static String TOKEN = "ya29.c.ElpsBrrj0GGyRPdYRoHQEbgUet5UZp47RTJMiEakVaOW1wIDX5Dmjo4pcAPLkUk01Eka5eJL4ScwQpIF50T9PiuN-_DRxqCyAp5F1MqEY_Cmvx9w_AK9n3QOmS4";
+	static final String JSON_FILE_2 = "src/test/resources/translate-api2-negative.json";
 
-	@Test
+	@Test(enabled=false)
 	public void traslateTest() throws Exception {
 		JSONParser parser = new JSONParser();
 		Object obj = parser.parse(new FileReader(JSON_FILE));
 
         JSONObject jsonObject = (JSONObject) obj;
                 
-		Response response = 
 		given().
-			header("Authorization", "Bearer " + TOKEN).
 			header("contentType", "application/json").and().
 			body(jsonObject.toJSONString()).
         when().
-        	post("https://translation.googleapis.com/language/translate/v2");
-		
-		if(response.statusCode() != 200) {
-			throw new Exception("Unexpected Response code: " + response.getStatusCode());
-		}
-		
-		ResponseBody responseBody = response.body();
-		responseBody.print();
-       
+        	post("https://translation.googleapis.com/language/translate/v2?key=AIzaSyD1ktJSjROx50db6zRP-iFPPBjxXcJCHx8").
+        then().
+        	statusCode(200).
+        	log().all();
 	}
+	
+	@Test
+	public void traslateTestNegative() throws Exception {
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(new FileReader(JSON_FILE_2));
+
+        JSONObject jsonObject = (JSONObject) obj;
+                
+		given().
+			header("contentType", "application/json").and().
+			body(jsonObject.toJSONString()).
+        when().
+        	post("https://translation.googleapis.com/language/translate/v2?key=AIzaSyD1ktJSjROx50db6zRP-iFPPBjxXcJCHx8").
+        then().
+        	statusCode(400).
+        	body("error.message", equalTo("Invalid Value")).
+        	log().all();
+	}
+	
 }
